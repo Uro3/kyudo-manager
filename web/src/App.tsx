4ae10364b47a2abf6b0c735a6f921fa990d2f02b
Uro3 <miyakawa.uro3@gmail.com>
2020-11-03 00:28:10 +0900

    add router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+import RecordList from './components/pages/RecordList';
 
-function App() {
+const AppRoute: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/' component={Home}></Route>
+      <Route path='/record' component={RecordList}></Route>
+    </Switch>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <AppRoute />
+    </BrowserRouter>
   );
 }
 
---
+++ web/src/components/pages/RecordList.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const RecordList: React.FC<{}> = () => {
+  return (
+    <div>
+      <h1>This is record list.</h1>
+    </div>
+  );
+};
+
+export default RecordList;
